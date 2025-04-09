05ec26fd7de0f8b506d2d03a178c26cd9e2fb4e4
flybenja <aldanabenjamin185@gmail.com>
2025-04-08 21:48:54 -0600

    Creacion de controlador para personas y modificacion de modelo de personas añadiendo relaciones

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -18,10 +18,37 @@ class Persona extends Model
         'fecha_nacimiento',
         'sexo',
         'num_telefono',
-        'tipo_persona',
+        'tipo_persona', 
         'padre_id',
         'madre_id',
         'padrino_id',
         'madrina_id',
     ];
+    public function municipio()
+    {
+        return $this->belongsTo(Municipio::class, 'municipio_id', 'municipio_id');
+    }
+     // Relación para el padre
+     public function padre()
+     {
+         return $this->belongsTo(self::class, 'padre_id');
+     }
+ 
+     // Relación para la madre
+     public function madre()
+     {
+         return $this->belongsTo(self::class, 'madre_id');
+     }
+ 
+     // Relación para el padrino
+     public function padrino()
+     {
+         return $this->belongsTo(self::class, 'padrino_id');
+     }
+ 
+     // Relación para la madrina
+     public function madrina()
+     {
+         return $this->belongsTo(self::class, 'madrina_id');
+     } 
 }
