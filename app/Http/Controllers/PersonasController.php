14815d7606dfb729973256e2ba4b73111d7468d3
kfajardop <kfajardop@miumg.edu.gt>
2025-04-08 19:05:08 -0600

    agregué el método index al controlador de personas

--- app/Http/Controllers/PersonasController.php
+++ app/Http/Controllers/PersonasController.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
 class PersonasController extends Controller
 {
-
-
+    public function index()
+    {
+        return view('personas.index');
+    }
 }
-
-
