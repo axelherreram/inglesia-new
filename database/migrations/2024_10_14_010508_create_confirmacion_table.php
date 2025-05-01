139d4ffd48f0f4baf78794d0cc191b2a7f0403fa
Huver <hdonisc1@miumg.edu.gt>
2025-04-30 22:43:25 -0600

    ajuste de la migracion de confirmaciones

--- database/migrations/2024_10_14_010508_create_confirmacion_table.php
+++ database/migrations/2024_10_14_010508_create_confirmacion_table.php
@@ -8,26 +8,30 @@ class CreateConfirmacionTable extends Migration
     public function up()
     {
         Schema::create('confirmacion', function (Blueprint $table) {
-            $table->increments('confirmacion_id');
+            $table->increments('confirmacion_id')->unsigned();
             $table->string('NoPartida', 20);
             $table->string('folio', 50);
             $table->dateTime('fecha_confirmacion');
-            $table->string('nombre_persona_confirmo', 255);
-            $table->string('nombre_persona_confirmada', 255);
-            $table->string('edad', 4);
+            $table->integer('persona_confirmada_id')->unsigned();
             $table->string('nombre_parroquia_bautizo', 255);
-            $table->string('nombre_padre', 255)->nullable();
-            $table->string('nombre_madre', 255)->nullable();
-            $table->string('nombre_persona_padrino', 255)->nullable();
-            $table->string('nombre_persona_madrina', 255)->nullable();
-            $table->integer('dato_parroquia_id')->unsigned();
             $table->integer('departamento_id')->unsigned();
             $table->integer('municipio_id')->unsigned();
-            $table->timestamps();
+            $table->integer('sacerdote_id')->unsigned()->comment('Referencia al sacerdote en la tabla personas');
+            $table->integer('padre_id')->unsigned()->nullable();
+            $table->integer('madre_id')->unsigned()->nullable();
+            $table->integer('padrino_id')->unsigned()->nullable();
+            $table->integer('madrina_id')->unsigned()->nullable();
+            $table->timestamps(0);
 
-            $table->foreign('dato_parroquia_id')->references('dato_parroquia_id')->on('dato_general_parroquia');
+            // Definición de claves foráneas
+            $table->foreign('persona_confirmada_id')->references('persona_id')->on('personas');
             $table->foreign('municipio_id')->references('municipio_id')->on('municipio');
             $table->foreign('departamento_id')->references('departamento_id')->on('departamento');
+            $table->foreign('sacerdote_id')->references('persona_id')->on('personas');
+            $table->foreign('padre_id')->references('persona_id')->on('personas');
+            $table->foreign('madre_id')->references('persona_id')->on('personas');
+            $table->foreign('padrino_id')->references('persona_id')->on('personas');
+            $table->foreign('madrina_id')->references('persona_id')->on('personas');
         });
     }
 
