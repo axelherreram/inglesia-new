88870eb520892e445c79a6639ba05989c90dfe06
Mynor Ceron <mcerong1@miumg.edu.gt>
2025-05-02 10:26:04 -0600

    Agregar /confirmaciones

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::middleware(['auth'])->group(function () {
     // Rutas para confirmaciones
     Route::get('/dashboard-list-confirmacion', [ConfirmacionController::class, 'index'])->name('confirmaciones.index');
     Route::get('/dashboard-confirmacion-create', [ConfirmacionController::class, 'create'])->name('confirmaciones.create');
-    // Route::post('/confirmaciones', [ConfirmacionController::class, 'store'])->name('confirmaciones.store');
+    Route::post('/confirmaciones', [ConfirmacionController::class, 'store'])->name('confirmaciones.store');
     // Route::get('/confirmaciones/{confirmacion_id}', [ConfirmacionController::class, 'show'])->name('confirmaciones.show');
     // Route::put('/confirmaciones/{confirmacion_id}', [ConfirmacionController::class, 'update'])->name('confirmaciones.update');
     // Route::get('/confirmacion/{confirmacion}/pdf', [ConfirmacionController::class, 'generatePDF'])->name('confirmacion.pdf');
