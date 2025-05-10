0e10d239f12b3e5a7591b4a487ffa97bf2eaa0a8
kfajardop <kfajardop@miumg.edu.gt>
2025-05-10 09:14:09 -0600

    arreglo de vistas

--- resources/views/bautizos/pdf.blade.php
+++ resources/views/bautizos/pdf.blade.php
@@ -4,10 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Constancia de Confirmación</title>
+    <title>Constancia de Bautizo</title>
     <link rel="stylesheet" href="{{ public_path('assets/css/font.css') }}">
-
-
     <style>
         body {
             font-family: 'Times New Roman', Times, serif;
@@ -85,32 +83,11 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
-            /* Alineación vertical de los elementos del formulario */
         }
 
         .form-group.date-group {
             justify-content: space-evenly;
         }
-
-        .signature-line-l {
-            position: absolute;
-            right: 40px;
-            width: 350px;
-            margin-top: 90px !important;
-            color: #3d69a8;
-            text-align: right;
-        }
-
-        .signature-line-l hr {
-            width: 100%;
-            border-top: 1px solid #3d69a8;
-        }
-
-        .signature-line-l p {
-            margin-top: 0px;
-            margin-right: 150px;
-        }
-
 
         .text-center {
             text-align: center;
@@ -132,14 +109,12 @@
         img.logo.right {
             right: 10px;
             height: 220px;
-
         }
 
         img.logo.cruzz {
             right: 10px;
             left: 10px;
             height: 220px;
-
         }
 
         span {
@@ -159,6 +134,26 @@
             font-size: 2.1rem;
             text-decoration: none !important;
         }
+
+
+        .signature-line-l {
+            position: absolute;
+            right: 40px;
+            width: 350px;
+            margin-top: 60px !important;
+            color: #3d69a8;
+            text-align: right;
+        }
+
+        .signature-line-l hr {
+            width: 100%;
+            border-top: 1px solid #3d69a8;
+        }
+
+        .signature-line-l p {
+            margin-top: 0px;
+            margin-right: 150px;
+        }
     </style>
 </head>
 
@@ -166,79 +161,79 @@
     <div class="certificate">
         <img src="{{ public_path('assets/img/logo_parroquia.png') }}" alt="Logo Parroquia" class="logo left">
         <img src="{{ public_path('assets/img/Mercedes.png') }}" alt="Mercedes" class="logo right">
-        <img src="{{ public_path('assets/img/Cruz.png') }}" alt="Mercedes" class="logo cruzz">
+        <img src="{{ public_path('assets/img/Cruz.png') }}" alt="Mercedes" class="logo  cruzz">
 
         <div class="header">
             <span class="h2">Parroquia Nuestra Señora de Las Mercedes</span>
             <p class="subtitle">Diócesis de Jalapa</p>
             <p class="subtitle">Calle al Calvario, Barrio el Centro, Sanarate, El Progreso</p>
-            <div class="title">CONSTANCIA DE CONFIRMACIÓN</div>
+            <div class="title">CONSTANCIA DE BAUTIZO</div>
         </div>
 
         <div class="form-content">
             <div class="form-group" style="width: 20cm;">
-                <label for="parroco">El infrascrito, Párroco de:</label>
-                <span>{{ $confirmacion->nombre_parroquia_bautizo }}</span>
+                <label for="parroco">El infrascrito, Párroco:</label>
+                <span>{{ $bautizo->sacerdote->nombres }} {{ $bautizo->sacerdote->apellidos }}</span>
             </div>
 
-            <p class="text-center" style="font-weight: 500; color: #3d6aa8de; font-size: 1.1rem;">
-                Certifica que en el libro de Confirmaciones:
-            </p>
-
+            <p class="text-center" style=" font-weight: 500;
+            color: #3d6aa8de;
+            font-size: 1.1rem;">Certifica que en
+                el libro de Bautizos:</p>
             <div class="form-group">
                 <label for="libro">No.:</label>
-                <span>{{ $confirmacion->NoPartida }}</span>
+                <span>{{ $bautizo->NoPartida }}</span>
                 <label for="folio" style="margin-left: 10px;">Folio:</label>
-                <span>{{ $confirmacion->folio }}</span>
+                <span>{{ $bautizo->folio }}</span>
                 <label style="margin-left: 10px;">de esta Parroquia consta que:</label>
             </div>
-
             <div class="form-group">
-                <span>{{ $confirmacion->personaConfirmada->nombres }}
-                    {{ $confirmacion->personaConfirmada->apellidos }}</span>
-            </div>
-
-            <div class="form-group">
-                <label for="padre">Hijo de</label>
-                <span>{{ $confirmacion->padre?->nombres ?? ' ' }} {{ $confirmacion->padre?->apellidos ?? '' }}</span>
+                <span>{{ $bautizo->personaBautizada->nombres }}
+                    {{$bautizo->personaBautizada->apellidos}}</span>
             </div>
             <div class="form-group">
-                <label for="">y de</label>
-                <span>{{ $confirmacion->madre?->nombres ?? ' ' }} {{ $confirmacion->madre?->apellidos ?? '' }}</span>
-            </div>
-
-            <div class="form-group">
-                <label for="nacido">Nacido el</label>
-                <span>{{ \Carbon\Carbon::parse($confirmacion->personaConfirmada->fecha_nacimiento)->locale('es')->isoFormat('D') }}</span>
+                <label for="nacimiento">Nacido el</label>
+                <span>{{ \Carbon\Carbon::parse($bautizo->personaBautizada->fecha_nacimiento)->locale('es')->isoFormat('D') }}</span>
                 <label for="mes" style="margin-left: 5px;">de</label>
-                <span>{{ \Carbon\Carbon::parse($confirmacion->personaConfirmada->fecha_nacimiento)->locale('es')->isoFormat('MMMM') }}</span>
+                <span>{{ \Carbon\Carbon::parse($bautizo->personaBautizada->fecha_nacimiento)->locale('es')->isoFormat('MMMM') }}</span>
                 <label for="ano" style="margin-left: 5px;">del año</label>
-                <span>{{ \Carbon\Carbon::parse($confirmacion->personaConfirmada->fecha_nacimiento)->locale('es')->isoFormat('Y') }}</span>
+                <span>{{ \Carbon\Carbon::parse($bautizo->personaBautizada->fecha_nacimiento)->locale('es')->isoFormat('Y') }}</span>
+            </div>
+
+            <div class="form-group">
+                <label for="dia">Fue bautizad@ el</label>
+                <span>{{ \Carbon\Carbon::parse($bautizo->fecha_bautizo)->locale('es')->isoFormat('D') }}</span>
+                <label for="mes" style="margin-left: 5px;">de</label>
+                <span>{{ \Carbon\Carbon::parse($bautizo->fecha_bautizo)->locale('es')->isoFormat('MMMM') }}</span>
+                <label for="ano" style="margin-left: 5px;">de</label>
+                <span>{{ \Carbon\Carbon::parse($bautizo->fecha_bautizo)->locale('es')->isoFormat('Y') }}</span>
             </div>
 
 
             <div class="form-group">
-                <label for="confirmado">Fue confirmado el</label>
-                <span>{{ \Carbon\Carbon::parse($confirmacion->fecha_confirmacion)->format('d') }}</span>
-                <label for="lugarConf">en</label>
-                <span>{{ $confirmacion->nombre_parroquia_bautizo }}</span>
+                <label>hijo de</label>
+                <span>{{ $bautizo->padre?->nombres ?? ' ' }} {{ $bautizo->padre?->apellidos ?? '' }}</span>
+            </div>
+            <div class="form-group">
+                <label>y de</label>
+                <span>{{ $bautizo->madre?->nombres ?? ' ' }} {{ $bautizo->madre?->apellidos ?? '' }}</span>
             </div>
 
             <div class="form-group">
-                <label for="padre">Por el Excmo. Monseñor</label>
-                <span>{{ $confirmacion->sacerdote->nombres }} {{ $confirmacion->sacerdote->apellidos }}</span>
-            </div>
-
-            <div class="form-group">
-                <label for="adrin">Habiendo sido padrino y madrina:</label>
+                <label>Habiendo sido madrina y padrino</label>
                 <span>
-                    {{ $confirmacion->padrino?->nombres ?? ' ' }} {{ $confirmacion->padrino?->apellidos ?? '' }}
-                    y
-                    {{ $confirmacion->madrina?->nombres ?? ' ' }} {{ $confirmacion->madrina?->apellidos ?? '' }}
+                    {{ $bautizo->padrino?->nombres ?? ' ' }} {{ $bautizo->padrino?->apellidos ?? '' }}
+                    <label style="margin: 0 5px;">y</label>
+                    {{ $bautizo->madrina?->nombres ?? ' ' }} {{ $bautizo->madrina?->apellidos ?? '' }}
                 </span>
             </div>
 
-            <div class="form-group date-group" style="margin-top: 50px ;">
+            <div class="form-group">
+                <label>Margen:</label>
+                <span>{{ $bautizo->margen }}</span>
+            </div>
+
+            <div class="form-group date-group" style="width: 16.59cm !important; margin-top:45px;">
                 <span class="day">{{ now()->format('d') }}</span>
                 <label>. de </label>
                 <span class="month">{{ now()->locale('es')->isoFormat('MMMM') }}</span>
