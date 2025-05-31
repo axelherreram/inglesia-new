bc2f73d9c4698252875c39ffd176c36c2ab401b1
kfajardop <kfajardop@miumg.edu.gt>
2025-05-30 23:52:27 -0600

    corrección de los comentarios de error al idioma español

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'required' => 'El campo :attribute es obligatorio.',
+    'exists' => 'El :attribute seleccionado no es válido.',
+    'string' => 'El campo :attribute debe ser una cadena de texto.',
+    'max' => [
+        'string' => 'El campo :attribute no puede exceder :max caracteres.',
+    ],
+    'date' => 'El campo :attribute debe ser una fecha válida.',
+    'before_or_equal' => 'El campo :attribute debe ser una fecha igual o anterior a hoy.',
+
+    'attributes' => [
+        'persona_participe_id' => 'persona que recibe la comunión',
+        'NoPartida' => 'número de libro',
+        'folio' => 'folio',
+        'fecha_comunion' => 'fecha de comunión',
+        'municipio_id' => 'municipio',
+        'departamento_id' => 'departamento',
+        'sacerdote_id' => 'sacerdote',
+        'padre_id' => 'padre',
+        'madre_id' => 'madre',
+        'esposa_id' => 'esposa',
+        'esposo_id' => 'esposo',
+    ],
+];
